f575799625e48adb18fef1a1b1a005561c7d6efe
G O <ryer168@gmail.com>
2018-09-26 11:22:16 -0400

    comm

--- hw1/NYUCodebase/NYUCodebase/main.cpp
+++ hw1/NYUCodebase/NYUCodebase/main.cpp
@@ -5,6 +5,9 @@
 #include <SDL_opengl.h>
 #include <SDL_image.h>
 
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 #include "ShaderProgram.h"
 
 #ifdef _WINDOWS
@@ -18,7 +21,7 @@ SDL_Window* displayWindow;
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("GO", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
 
@@ -26,22 +29,54 @@ int main(int argc, char *argv[])
     glewInit();
 #endif
 
-	glViewport(0, 0, 1280, 720);
+	//setup
+
+	glViewport(0, 0, 640, 480);
 
 	ShaderProgram program;
 	program.Load(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragnebt.glsl");
 
 	glm::mat4 projectionMatrix = glm::mat4(1.0f);
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	glm::mat4 viewMatrix = glm::mat4(1.0f);
+
+	projectionMatrix = glm::ortho(-1.777f, 1.777f, -1.0f, 1.0f, -1.0f, 1.0f);
+
+	glUseProgram(program.programID);
 
     SDL_Event event;
     bool done = false;
+
+	//Time Code
+	float lastFrameTicks = 0.0f;
+
     while (!done) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
                 done = true;
             }
         }
+
+		//Time code
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+
+
         glClear(GL_COLOR_BUFFER_BIT);
+
+		//loop code
+		program.SetModelMatrix(modelMatrix);
+		program.SetProjectionMatrix(projectionMatrix);
+		program.SetViewMatrix(viewMatrix);
+
+		float vertices[] = { 0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f };
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+		glEnableVertexAttribArray(program.positionAttribute);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(program.positionAttribute);
+
         SDL_GL_SwapWindow(displayWindow);
     }
     
